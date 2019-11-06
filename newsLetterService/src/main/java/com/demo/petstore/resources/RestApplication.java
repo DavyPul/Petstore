ad26057676ca2b50f972a4c0a25941bfdbca34ae
PULINCKX DAVY M.A <davy.pulinckx@minfin.fed.be>
2019-11-06 22:28:29 +0100

    init war

---
+++ newsLetterService/src/main/java/com/demo/petstore/resources/RestApplication.java
@@ -0,0 +1,11 @@
+package com.demo.petstore.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+    public RestApplication() {}
+
+}
