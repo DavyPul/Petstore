00ea4cc5e7ecff7d62be3f7aa5737d751ae2fb66
DavyPul <davy.pulinckx@gmail.com>
2019-11-09 19:47:39 +0100

    update

--- NewsLetterService/src/main/java/com/demo/petstore/resources/NewsLetterResources.java
+++ NewsLetterService/src/main/java/com/demo/petstore/resources/NewsLetterResources.java
@@ -1,4 +1,32 @@
 package com.demo.petstore.resources;
 
+
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("newsletter")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Stateless
 public class NewsLetterResources {
+
+    @GET
+    @Path("ping")
+    public Response pingResource() {
+        return Response.status(Response.Status.OK).build();
+    }
+
+
+    // TODO save email for newsletter
+
+
+    // TODO remove email from newsletter
+
+
+    // TODO retrieve all the members of the newsletter
+
+
+
 }
